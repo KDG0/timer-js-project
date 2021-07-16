078f7e8bb0414983e4688e4cb1dcfa9125308243
KDG0 <kedav26@gmail.com>
2021-07-15 21:33:53 -0600

    added start method & querySelectors

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ class Timer {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+    }
+    start(){
+        console.log('Time to start the timer!');
     }
 }
